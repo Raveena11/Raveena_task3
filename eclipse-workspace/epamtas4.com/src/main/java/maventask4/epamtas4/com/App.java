a1a3ea0827191b0be3ae85c4a58291bc5367d8be
Raveena11 <raveenab113@gmail.com>
2020-02-20 21:53:59 +0530

    first commit on construction

---
+++ eclipse-workspace/epamtas4.com/src/main/java/maventask4/epamtas4/com/App.java
@@ -0,0 +1,13 @@
+package maventask4.epamtas4.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ eclipse-workspace/epamtas4.com/src/main/java/maventask4/epamtas4/com/Structure.java
@@ -0,0 +1,9 @@
+package maventask4.epamtas4.com;
+
+public class Structure {
+	public static void main(String[] args) {
+		HouseConstruction hs = new HouseConstruction();
+		hs.dimensions();
+		}
+
+}
